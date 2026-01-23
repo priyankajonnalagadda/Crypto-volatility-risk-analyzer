cc6947c587f6cd93e48e4bcf401e34d6137ae8e4
Priyanka <priyankajonnalagadda0@gmail.com>
2026-01-23 13:32:58 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+# Add project root to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import streamlit as st
 from data.data_loader import load_bitcoin
 from volatility.volatility_calc import volatility_analysis
